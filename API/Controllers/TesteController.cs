66a98b2ae23f18ec3b8aef06fe3245fb75dbc2ee
Matheus-Sleutjes <83664192+Matheus-Sleutjes@users.noreply.github.com>
2022-08-24 18:54:23 -0400

    ajuste controller

--- API/Controllers/TesteController.cs
+++ API/Controllers/TesteController.cs
@@ -4,7 +4,7 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class TesteController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
